8efc5604b110bd8325aafc9ef3230b54fa0b1d82
khh <khhcjswo212@naver.com>
2017-07-31 15:52:29 +0900

    commit_add

---
+++ app/src/main/java/com/example/samsung/hilaris/Week_CheckableLinearLayout.java
@@ -0,0 +1,38 @@
+package com.example.samsung.hilaris;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.CheckBox;
+import android.widget.Checkable;
+import android.widget.LinearLayout;
+
+/**
+ * Created by SAMSUNG on 2017-07-31.
+ */
+
+public class Week_CheckableLinearLayout extends LinearLayout implements Checkable{
+
+    public Week_CheckableLinearLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public void setChecked(boolean checked) {
+        CheckBox day_box = (CheckBox)findViewById(R.id.week_check);
+        if(day_box.isChecked() != checked){
+            day_box.setChecked(checked);
+        }
+    }
+
+    @Override
+    public boolean isChecked() {
+        CheckBox day_box = (CheckBox)findViewById(R.id.week_check);
+        return day_box.isChecked();
+    }
+
+    @Override
+    public void toggle() {
+        CheckBox day_box = (CheckBox)findViewById(R.id.week_check);
+        setChecked(day_box.isChecked() ? false : true);
+    }
+}
